41c15162c4ad575e6341c23845433e6ecea33207
Doukoure et Assale <doukouremoriba25@gmail.com>
2023-09-07 11:38:01 +0200

    avancement sur le front

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -5,8 +5,13 @@ export default {
       question: [],
       index: 0,
       currentQuestion: {}, // Ajout de  cette propriété pour stocker la question actuelle
+      anwsers: [],// Tableau pour stocker les réponses de l'utilisateur
+      A:"",
+      B:"",
+      C:"",
     };
   },
+
 
   methods: {
     // Fonction pour récuperer la liste des questions des que l'utilisateur charge la page.
@@ -23,6 +28,8 @@ async getQuestion() {
              this.question = res.data; // Recuperation de toute les infos dans la table Question depuis l'api.
           }
     },
+    
+
     
      // Fonction pour afficher la question actuelle
     displayCurrentQuestion() {
@@ -41,6 +48,47 @@ async getQuestion() {
       }
     },
   },
+
+
+
+  
+  //  // Méthode pour soumettre les réponses
+  //   submitAnswers() {
+  //     // Affichez les réponses dans la console (vous pouvez également les envoyer au serveur ici)
+  //     console.log("Réponses soumises :", this.answers);
+  //     // Réinitialisez les réponses pour la prochaine utilisation
+  //     this.answers = {
+  //       A: null,
+  //       B: "",
+  //       C: null,
+  //     };
+  //   },
+
+// Fonction pour enregistrer les réponses
+  saveResponse() {
+    // Créez un objet pour stocker la réponse de l'utilisateur
+    const response = {
+      questionId: this.currentQuestion.id, // L'ID de la question actuelle (ajoutez une propriété ID à vos questions si ce n'est pas déjà fait)
+      responseA: this.A, // La réponse à la question de type A
+      responseB: this.B, // La réponse à la question de type B
+      responseC: this.C, // La réponse à la question de type C
+    };
+
+    // Vous pouvez maintenant sauvegarder cet objet de réponse où vous le souhaitez, par exemple en le stockant dans un tableau
+    this.answers.push(response);
+
+    // Réinitialisez les champs de réponse pour la prochaine question
+    this.A = "";
+    this.B = "";
+    this.C = "";
+
+    // Appelez ensuite la fonction nextQuestion() pour passer à la question suivante
+    this.nextQuestion();
+  },
+
+  // ...
+
+
  async created() {
   await this.getQuestion(); // Attendons que la requête soit terminée
   this.displayCurrentQuestion(); // Ensuite on affiche la première question une fois que les données sont disponibles
@@ -65,17 +113,17 @@ async getQuestion() {
       <!-- question de type A-->
       <div class="containerTypeA" v-if="currentQuestion.question_type=='A'">
        <li v-for=" index in currentQuestion.propositions" :key="index">
-        <input type="radio">{{ index }}
+       <input type="radio" v-model="A" :value="index">{{ index }}
       </li>
 
       </div>
       <!-- question de type B-->
       <div class="containerTypeB" v-if="currentQuestion.question_type=='B'">
-        <input type="text">
+        <input type="text" v-model="B">
       </div>
       <!-- question de type C-->
       <div class="containerTypeC" v-if="currentQuestion.question_type=='C'">
-        <input type="number">
+        <input type="number" v-model="C">
       </div>
     </div>
 
@@ -83,6 +131,10 @@ async getQuestion() {
       <button class="arrow-next" @click="nextQuestion()">
         <i class="fa-solid fa-arrow-right"></i>
       </button>
+
+       <!-- Bouton de soumission -->
+    <!-- <button type="submit" @click="submitAnswers()">Soumettre</button> -->
+  
     </div>
   </div>
 </template>
