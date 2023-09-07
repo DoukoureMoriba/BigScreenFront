7a979a6c32eff961c6550ec17464173fb8efae5a
Doukoure et Assale <doukouremoriba25@gmail.com>
2023-09-07 16:41:20 +0200

    recup des réponses terminé j'attque la soumission

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -5,10 +5,12 @@ export default {
       question: [],
       index: 0,
       currentQuestion: {}, // Ajout de  cette propriété pour stocker la question actuelle
-      anwsers: [],// Tableau pour stocker les réponses de l'utilisateur
+      answers: [],// Tableau pour stocker les réponses de l'utilisateur
       A:"",
       B:"",
       C:"",
+      showNextButton: true, // Afficher par défaut le bouton "Next Question"
+      showFinalizeButton: false, // Ajoutez cette propriété
     };
   },
 
@@ -16,7 +18,9 @@ export default {
   methods: {
     // Fonction pour récuperer la liste des questions des que l'utilisateur charge la page.
 
-async getQuestion() {
+async getQuestion()  {
+
+
       var url = "http://127.0.0.1:8000/api/questionList";
       var res = await(await fetch(url, {
          method: "GET",
@@ -26,6 +30,20 @@ async getQuestion() {
 
        if (res.status == "Done") {
              this.question = res.data; // Recuperation de toute les infos dans la table Question depuis l'api.
+          }
+    },
+
+    
+    async submitResp() {
+     var url = "http://127.0.0.1:8000/api/submitResponses";
+    var res = await(await fetch(url, {
+         method: "POST",
+        headers: { "Content-Type": "application/json" },
+      }) 
+      ).json();
+
+       if (res.status == "Done") {
+             this.question = res.data; // Recuperation de toute les infos dans la table responses depuis l'api.
           }
     },
     
@@ -38,60 +56,78 @@ async getQuestion() {
       } else {
         this.currentQuestion = {}; // rénitialisation de la question si la liste est vide
       }
+
+  //     // Vérifiez si l'utilisateur est à la dernière question
+  // if (this.index === this.question.length -1) {
+  //   this.showFinalizeButton = true;
+  // } else {
+  //   this.showFinalizeButton = false;
+  // }
     },
     
     // Fonction pour passer à la question suivante
-    nextQuestion() {
-      if (this.index < this.question.length - 1) {
-        this.index++;
-        this.displayCurrentQuestion();
-      }
-    },
-  },
+   nextQuestion() {
+  if (this.index < this.question.length - 1) {
+    this.index++;
+    this.displayCurrentQuestion();
+    this.showNextButton = true;
+    this.showFinalizeButton = false;
+  } else {
+    // Si l'utilisateur est à la dernière question, affichez le bouton "Finalisez"
+    this.showNextButton = false;
+    this.showFinalizeButton = true;
+  }
+},
 
-
-
-  
-  //  // Méthode pour soumettre les réponses
-  //   submitAnswers() {
-  //     // Affichez les réponses dans la console (vous pouvez également les envoyer au serveur ici)
-  //     console.log("Réponses soumises :", this.answers);
-  //     // Réinitialisez les réponses pour la prochaine utilisation
-  //     this.answers = {
-  //       A: null,
-  //       B: "",
-  //       C: null,
-  //     };
-  //   },
-
-// Fonction pour enregistrer les réponses
-  saveResponse() {
+    saveResponse() {
     // Créez un objet pour stocker la réponse de l'utilisateur
     const response = {
-      questionId: this.currentQuestion.id, // L'ID de la question actuelle (ajoutez une propriété ID à vos questions si ce n'est pas déjà fait)
-      responseA: this.A, // La réponse à la question de type A
-      responseB: this.B, // La réponse à la question de type B
-      responseC: this.C, // La réponse à la question de type C
+        questionId: this.currentQuestion.id, // L'ID de la question actuelle (ajoutez une propriété ID à vos questions si ce n'est pas déjà fait)
+        userResponse: "",
     };
+
+    // Stockez la réponse dans l'objet answers en fonction du type de question
+    if (this.currentQuestion.question_type === 'A') {
+        response.userResponse = this.A;
+    } else if (this.currentQuestion.question_type === 'B') {
+        response.userResponse = this.B;
+    } else if (this.currentQuestion.question_type === 'C') {
+        response.userResponse = this.C;
+    }
 
     // Vous pouvez maintenant sauvegarder cet objet de réponse où vous le souhaitez, par exemple en le stockant dans un tableau
     this.answers.push(response);
+    console.log(this.answers);
 
     // Réinitialisez les champs de réponse pour la prochaine question
     this.A = "";
     this.B = "";
     this.C = "";
 
-    // Appelez ensuite la fonction nextQuestion() pour passer à la question suivante
-    this.nextQuestion();
+    // Maintenant, vous pouvez utiliser une promesse ou une callback pour appeler nextQuestion une fois la réponse sauvegardée.
+    this.continuerVersQuestionSuivante().then(() => {
+        this.nextQuestion();
+    });
+},
+
+continuerVersQuestionSuivante() {
+    return new Promise((resolve) => {
+        // Vous pouvez ajouter des opérations asynchrones ici si nécessaire
+        resolve();
+    });
+},
+
+
+
   },
 
-  // ...
+
 
 
  async created() {
   await this.getQuestion(); // Attendons que la requête soit terminée
   this.displayCurrentQuestion(); // Ensuite on affiche la première question une fois que les données sont disponibles
+  await this.submitResp(); // Attendons que la requête soit terminée
 },
 };
 </script>
@@ -128,9 +164,13 @@ async getQuestion() {
     </div>
 
     <div class="question_footer">
-      <button class="arrow-next" @click="nextQuestion()">
+      <button class="arrow-next"  @click="saveResponse" v-if="showNextButton">
         <i class="fa-solid fa-arrow-right"></i>
       </button>
+
+
+<button type="submit" v-if="showFinalizeButton" @click="submitResp()" >Finalisez</button>
+
 
        <!-- Bouton de soumission -->
     <!-- <button type="submit" @click="submitAnswers()">Soumettre</button> -->
